7cd82a5394c10f4f7891988375566b785d13d72a
Satshabad Khalsa <satshabad.music@gmail.com>
2013-02-27 22:33:05 -0800

    prototype

---
+++ jaspect.js
@@ -0,0 +1,29 @@
+var scripts = top.document.getElementsByTagName('script');
+
+get users script
+
+for scripts: get all scripts:
+    run and replace
+
+delete original scripts
+insert new scripts
+
+
+foo(x,y,z)
+
+before_foo(foo,x,y,z)
+
+
+before_foo(, argsf){
+
+code
+
+foo(xy,z,)
+}
+
+
+around_pointcut(
+
+code
+
+)

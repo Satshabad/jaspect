26cafffc05aeea7c186160e0d9c03b63362715ae
Satshabad Khalsa <satshabad.music@gmail.com>
2013-02-28 21:39:21 -0800

    basic setup for burrito

--- jaspect.js
+++ jaspect.js
@@ -1,29 +1,11 @@
+
+
 var scripts = top.document.getElementsByTagName('script');
 
-get users script
-
-for scripts: get all scripts:
-    run and replace
-
-delete original scripts
-insert new scripts
 
 
-foo(x,y,z)
+var src = burrito('f() && g(h())\nfoo()', function (node) {
+    if (node.name === 'call') node.wrap('qqq(%s)');
+});
 
-before_foo(foo,x,y,z)
-
-
-before_foo(, argsf){
-
-code
-
-foo(xy,z,)
-}
-
-
-around_pointcut(
-
-code
-
-)
+console.log(src);

3013a6a058dc51bb2603888cc16c9f4d1d069174
Satshabad Khalsa <satshabad.music@gmail.com>
2013-03-03 12:47:26 -0800

    outlined how module will be organized

--- cli.js
+++ cli.js
@@ -1,1 +1,3 @@
 // This file will contain the logic that interfaces with core.js to process a js file on the server side.
+
+var jaspect = require("./core");
--- core.js
+++ core.js
@@ -1,31 +1,48 @@
 // This file will conatain all the core logic of jaspect. For now it's a staging designed to work with index.html
 
-var scripts = top.document.getElementsByTagName('script');
+var burrito = require('burrito');
+var parse = burrito.parse;
 
-
-
-//console.log(parse('function foo(x, y, z){\n return x + y + z; \n}'));
-//console.log(deparse(["toplevel", [ ["defun", "foo" ,["x","y","z"],[["return", [ "binary", "+", [ "binary", "+", ["name", "x"],["name", "y"]],["name","z"]]]]]]]))
-
-function traverse(tree){
-	
-  if (typeof tree === 'string'){
-    return;
-  	}
+module.exports = function(files){
+	jaspect = {};
   
-  for (var i = 0; i < tree.length; i++){
-    
-    if (tree[i] === 'defun'){
-      tree[3].unshift(["stat", ["call", ["name", "bar"], []]]);
+  
+  jaspect.files = files;
+  
+  
+  var traverse = function(tree){
+  
+    if (typeof tree === 'string'){
+      return;
     }
-    traverse(tree[i]);
+    
+    for (var i = 0; i < tree.length; i++){
+      
+      if (tree[i] === 'defun'){
+        tree[3].unshift(["stat", ["call", ["name", "bar"], []]]);
+      }
+      traverse(tree[i]);
+    }
+  
   }
+  
 
+  jaspect.after = function(pointcut, callback){
 
+  }
+  
+  jaspect.before = function (pointcut, callback){
+
+  }
+    
+  jaspect.around =  function(pointcut, callback){
+
+  }
+    
+    
+    
+  return jaspect;
 }
 
-ast = parse('function foo(x, y, z){\n return x + y + z; \n}');
-console.log(ast);
-traverse(ast);
-console.log(ast);
-console.log(deparse(ast));
+  
+

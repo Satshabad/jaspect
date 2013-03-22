bebffc200e334c42d9a4103f4bdee5ed829ccf60
Satshabad Khalsa <satshabad.music@gmail.com>
2013-03-21 22:15:40 -0700

    rewrote all to use mocha

--- tests/tests.js
+++ tests/tests.js
@@ -38,16 +38,16 @@ suite("Helpers", function () {
 
   });
 
-  suite("ReplaceDeepestCall", function () {
+  suite("replaceDeepestCall", function () {
 
-    test('replaceDeepestCall should replace the deepest call node with the given one', function () {
+    test('should replace the deepest call node with the given one', function () {
 
       assert.deepEqual(replaceDeepestCall(parseSingleStat("foo().bar.baz();") ,["name", "__t0"]), 
                    parseSingleStat("__t0.bar.baz()"));
 
     });
 
-    test('replaceDeepestCall should handle single call cases', function () {
+    test('should handle single call cases', function () {
 
       assert.deepEqual(replaceDeepestCall(parseSingleStat("foo();") ,["name", "__t0"]), 
                    parseSingleStat("__t0"));
@@ -55,7 +55,7 @@ suite("Helpers", function () {
     });
 
 
-    test('replaceDeepestCall should handle proper order of arguments', function () {
+    test('should handle proper order of arguments', function () {
 
       assert.deepEqual(replaceDeepestCall(parseSingleStat("foo(baz(), x, bar())"),["name", "__t0"]), 
                    parseSingleStat("foo(__t0, x, bar())"));
@@ -67,136 +67,68 @@ suite("Helpers", function () {
   
 });
 
-exports.testHelpers = function(test){
+suite("tacifyNested", function () {
 
-  test.expect(6);
-
-	var input = parseSingleStat("foo().bar.baz();");
-  var expected = parse("__t0.bar.baz();");
-  var e = deparse(expected);
-  var o = deparse(tacifyFunctions.replaceCall(input, ["name", "__t0"], 4));
-  test.equal(e, o);
-  
-	var input = parseSingleStat("foo().bar.baz();");
-  var expected = 4;
-  var o = tacifyFunctions.findDepthOfDeepestCall(input);
-  test.equal(expected, o);
-
-	var input = parseSingleStat("foo().bar.baz();");
-  var expected = parseSingleStat("foo()")[1];
-  var o = deparse(tacifyFunctions.findDeepestCall(input));
-  var e = deparse(expected);
-  test.equal(e, o);
-
-	var input = parseSingleStat("foo();");
-  var expected = parseSingleStat("__t0");
-  var e = deparse(expected);
-  var o = deparse(tacifyFunctions.replaceDeepestCall(input,["name", "__t0"]));
-  test.equal(e, o);
-
- 	var input = parseSingleStat("foo().bar.baz();");
-  var expected = parseSingleStat("__t0.bar.baz()");
-  var e = deparse(expected);
-  var o = deparse(tacifyFunctions.replaceDeepestCall(input,["name", "__t0"]));
-  test.equal(e, o); 
-
- 	var input = parseSingleStat("foo(baz(), x, bar())");
-  var expected = parseSingleStat("foo(__t0, x, bar())");
-  var e = deparse(expected);
-  var o = deparse(tacifyFunctions.replaceDeepestCall(input,["name", "__t0"]));
-  test.equal(e, o); 
+  test("should tacify dependent calls", function () {
+    assert.deepEqual(tacifyNested(parse("x = obj.bar().baz();")), 
+                     parse("var __t0 = obj.bar(); x = __t0.baz();"));
+  });
 
 
-  test.done();
+  test("should not tacify nodes with no calls", function () {
+    assert.deepEqual(tacifyNested(parse("x = obj.bar.baz();")), 
+                     parse("x = obj.bar.baz();"));
+  });
 
-}
+  test("should tacify nested calls", function () {
+    assert.deepEqual(tacifyNested(parse("foo(baz(), x, bar())")), 
+             parse("var __t0 = baz(); var __t1 = bar(); foo(__t0, x, __t1);"));
+  }); 
+
+  test("should tacify object literal calls in order", function () {
+    assert.deepEqual(tacifyNested(parse("var x = { foo: bar(), bin: baz() };")), 
+             parse("var __t0 = bar(); var x = { foo : __t0 , bin: baz() };"));
+
+
+  }); 
+
+});
+
+
+suite("tacifyWhile", function () {
+
+  test("should tacify conditional but not block", function () {
+
+    assert.deepEqual(tacifyWhile(parseSingleStat("while(foo() < 1){bar();}")), 
+                     parse("var __t0 = foo(); while(__t0 < 1){bar(); var __t0 = foo(); }"));
+  });
+
+});
 
 
 
+suite("tacifyFor", function () {
 
-exports.testNested = function(test){
-  test.expect(4);
-  
-	var input = parse("x = obj.bar().baz();");
-  var expected = parse("var __t0 = obj.bar();\
-									    x = __t0.baz();");
-
-  test.equal(deparse(expected), deparse(tacifyFunctions.tacifyNested(input)));
-  
-
-  input = parse("foo(baz(), x, bar())");
-  expected = parse("var __t0 = baz();\
-                  var __t1 = bar();\
-                  foo(__t0, x, __t1);");
-
-  test.equal(deparse(expected), deparse(tacifyFunctions.tacifyNested(input)));
-  
-
-  input = parse("x = obj.bar.baz();");
-  expected = parse("x = obj.bar.baz();");
-
-  test.equal(deparse(expected), deparse(tacifyFunctions.tacifyNested(input)));
-
-
-  input = parse("var x = { foo: bar(), bin: baz() };");
-  expected = parse("var __t0 = bar();\
-                  var x = { foo : __t0 , bin: baz() };");
-
-  test.equal(deparse(expected), deparse(tacifyFunctions.tacifyNested(input)));
-  test.done();
-
-
-};
-
-exports.testWhile = function(test){
-
-  test.expect(1);
-  
-	var input = parseSingleStat("while(foo() < 1){bar();}");
-  var expected = parse("var __t0 = foo(); while(__t0 < 1){bar(); var __t0 = foo(); }");
-  var e = deparse(expected);
-  var o = deparse(tacifyFunctions.tacifyWhile(input));
-  test.equal(e, o);
-  
-
-  test.done();
-
-
-}
-
-exports.testFor = function(test){
-
-  test.expect(2);
-  
-	var input = parseSingleStat("for(var i = bar();foo() < 1; i = baz()){x = 2;}");
-  var expected = parse("var __t0 = bar(); var __t1 = foo(); var __t2 = baz(); \
+  test("should tacify conditionals",  function () {
+    assert.deepEqual(tacifyFor(parseSingleStat("for(var i = bar();foo() < 1; i = baz()){x = 2;}")), 
+                    parse("var __t0 = bar(); var __t1 = foo(); var __t2 = baz(); \
                       for(var i = __t0; __t1 < 1; i = __t2){ \
-                        x = 2; var __t0 = bar(); var __t1 = foo(); var __t2 = baz()}");
-  var e = deparse(expected);
-  var o = deparse(tacifyFunctions.tacifyFor(input));
-  test.equal(e, o);
-  
-  var input = parseSingleStat("for(var i = 0; foo() < 1; i = baz().bar()){x = 2;}");
-  var expected = parse("var __t0 = foo(); var __t1 = baz(); var __t2 = __t1.bar(); \
+                        x = 2; var __t0 = bar(); var __t1 = foo(); var __t2 = baz()}"));
+  });
+
+  test("should tacify nested calls in conditionals", function () {
+    assert.deepEqual(tacifyFor(parseSingleStat("for(var i = 0; foo() < 1; i = baz().bar()){x = 2;}")), 
+                    parse("var __t0 = foo(); var __t1 = baz(); var __t2 = __t1.bar(); \
                       for(var i = 0; __t0 < 1; i = __t2){ \
-                        x = 2; var __t0 = foo(); var __t1 = baz(); var __t2 = __t1.bar(); }");
-  var e = deparse(expected);
-  var o = deparse(tacifyFunctions.tacifyFor(input));
-  test.equal(e, o);
-  
+                        x = 2; var __t0 = foo(); var __t1 = baz(); var __t2 = __t1.bar(); }"));
+  });
 
-  test.done();
+});
 
+suite("tacify", function () {
+  test("tacify should work with nested loops", function () {
 
-}
-
-
-// exports.testTacify = 
- x = function(test){
-
-  test.expect(1);
-  
-	var input = parse(heredoc(function () {/*
+   var input = parse(heredoc(function () {/*
 	                    w = 3;
                       while(foo()){
                         x = bar().baz();
@@ -222,17 +154,9 @@ exports.testFor = function(test){
                         }
                         var __t0 = foo();
                       }
- */ })); 
-  var e = deparse(expected);
-  var o = deparse(tacify(input));
-  test.equal(e, o);
-  
+ */ }));  
 
-  test.done();
+  assert.deepEqual(tacify(input), expected);
 
-
-}
-
-
-
-
+  });
+});
